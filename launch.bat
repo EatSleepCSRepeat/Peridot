13296b425b0548d9e8261fabe6f7a3bc5272691c
Eat, Sleep, C#, Repeat <163324856+EatSleepCSRepeat@users.noreply.github.com>
2025-03-29 11:41:29 -0400

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -4,4 +4,8 @@ python -c "import requests" >nul 2>nul
 if %errorlevel% neq 0 (
     pip install requests
 )
+python -c "import colorama" >nul 2>nul
+if %errorlevel% neq 0 (
+    pip install colorama
+)
 python ./src/peridot.py
